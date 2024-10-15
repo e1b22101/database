1059be1055fd4f4fb8956dec6d4bdac7ebf869af
e1b22057 <e1b22057@oit.ac.jp>
2024-10-15 16:08:53 +0900

    selectで1つだけ取得して表示させる

--- src/main/java/team5/database/controller/Sample41Controller.java
+++ src/main/java/team5/database/controller/Sample41Controller.java
@@ -13,10 +13,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-//import oit.is.inudaisuki.springboot_samples.model.Chamber;
-//import oit.is.inudaisuki.springboot_samples.model.ChamberMapper;
-//import oit.is.inudaisuki.springboot_samples.model.ChamberUser;
-//import oit.is.inudaisuki.springboot_samples.model.UserInfo;
+import team5.database.model.Chamber;
+import team5.database.model.ChamberMapper;
+//import team5.database.model.ChamberUser;
+//import team5.database.model.UserInfo;
 
 /**
  * /sample3へのリクエストを扱うクラス authenticateの設定をしていれば， /sample3へのアクセスはすべて認証が必要になる
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class Sample41Controller {
 
   @Autowired
-  //ChamberMapper chamberMapper;
+  ChamberMapper chamberMapper;
 
   @GetMapping("step1")
   public String sample41() {
@@ -46,5 +46,50 @@ public class Sample41Controller {
   @GetMapping("step6")
   public String sample46() {
     return "sample46.html";
+  }
+
+  /**
+   * 指定したidをPATHパラメータで受け取り，そのidに対応するデータを取得して返す
+   *
+   * @param id
+   * @param model
+   * @return
+   */
+  @GetMapping("step2/{id}")
+  public String sample42(@PathVariable Integer id, ModelMap model) {
+    Chamber chamber2 = chamberMapper.selectById(id);
+    model.addAttribute("chamber2", chamber2);
+
+    return "sample41.html";
+  }
+
+  /**
+   *
+   * @param model Thymeleafにわたすデータを保持するオブジェクト
+   * @param prin  ログインユーザ情報が保持されるオブジェクト
+   * @return
+   *
+   *         Transactionalはメソッドでトランザクション処理を実施したい場合に付与する
+   *         このメソッドが開始するとトランザクションが開始され，メソッドが正常に終了するとDBへのアクセスが確定する（Runtime
+   *         errorなどで止まった場合はロールバックが行われる）
+   */
+  @PostMapping("step3")
+  @Transactional
+  public String sample43(@RequestParam String chamberName, ModelMap model, Principal prin) {
+    String loginUser = prin.getName(); // ログインユーザ情報
+    Chamber chamber3 = new Chamber();
+    chamber3.setChamberName(chamberName);
+    chamber3.setUserName(loginUser);
+    chamberMapper.insertChamber(chamber3);
+    model.addAttribute("chamber3", chamber3);
+    // System.out.println("ID:" + chamber3.getId());
+    return "sample43.html";
+  }
+
+  @PostMapping("step5")
+  public String sample45(@RequestParam String chamberName, ModelMap model) {
+    ArrayList<Chamber> chambers5 = chamberMapper.selectAllByChamberName(chamberName);
+    model.addAttribute("chambers5", chambers5);
+    return "sample45.html";
   }
 }
